9ab5f6183dd2476e5e08610201d7d28834b53ee9
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-04-01 19:19:28 -0300

    Create loss_curve

---
+++ plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from network import RedeNeural
+from loss import mse
+
+X = np.array([
+    [0,0],
+    [0,1],
+    [1,0],
+    [1,1]])
+
+y = np.array([[0], [1], [1], [0]])
+
+losses = []
+
+net = RedeNeural(2, 4, 1)
+epochs = 10000
+lr = 0.1
+
+for epoch in range(epochs):
+    y_pred = net.forward(X)
+    loss = mse(y_pred, y)
+    net.backward(X, y, lr)
+    if epoch % 100 == 0:
+        losses.append(loss)
+
+plt.plot(losses)
+plt.title("Curva de perda")
+plt.xlabel("Epoch")
+plt.ylabel("MSE Loss")
+plt.savefig("loss_curve.png")
+plt.show()
